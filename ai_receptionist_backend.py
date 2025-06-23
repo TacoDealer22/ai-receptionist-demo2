a4e5662988d54ade32af0cbd65c4038ded0931a4
TacoDealer22 <aljalladomar@live.com>
2025-06-23 13:24:53 +0300

    Update ai_receptionist_backend.py

--- ai_receptionist_backend.py
+++ ai_receptionist_backend.py
@@ -62,5 +62,5 @@ def voice():
 def serve_static(filename):
     return send_from_directory("static", filename)
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+iif __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
